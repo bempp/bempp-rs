0a43b113c58f1d31a13703bdc73c96df629819db
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-12-19 15:41:29 +0000

    fmt

--- grid/src/grid.rs
+++ grid/src/grid.rs
@@ -1,4 +1,4 @@
 pub use solvers_traits::grid::Geometry;
+pub use solvers_traits::grid::Grid;
 pub use solvers_traits::grid::Locality;
 pub use solvers_traits::grid::Topology;
-pub use solvers_traits::grid::Grid;
