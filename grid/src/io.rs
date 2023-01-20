309004155f215d88eb46ccefe4f00c3413ffe963
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-01-20 16:42:49 +0000

    Support quadrilateral cells in io

--- grid/src/io.rs
+++ grid/src/io.rs
@@ -19,6 +19,9 @@ pub fn export_as_gmsh(grid: SerialGrid, fname: String) {
             let coord = grid.geometry().point(i).unwrap()[j];
             gmsh_s.push_str(&format!(" {coord}"));
         }
+        for j in grid.geometry().dim()..3 {
+            gmsh_s.push_str(&format!(" 0.0"));
+        }
         gmsh_s.push_str("\n");
     }
     gmsh_s.push_str("$EndNodes\n");
@@ -26,10 +29,22 @@ pub fn export_as_gmsh(grid: SerialGrid, fname: String) {
     let cell_count = grid.topology().entity_count(grid.topology().dim());
     gmsh_s.push_str(&format!("{cell_count}\n"));
     for i in 0..cell_count {
-        gmsh_s.push_str(&format!("{i} 2 2 0 0")); // TODO: 2 is hardcoded triangle
-        for j in 0..3 {
+        let cell = grid.topology().cell(i).unwrap();
+        gmsh_s.push_str(&format!("{i} "));
+        let mut vertex_order = vec![];
+        if cell.len() == 3 {
+            gmsh_s.push_str("2");
+            vertex_order = vec![0, 1, 2];
+        } else if cell.len() == 4 {
+            gmsh_s.push_str("3");
+            vertex_order = vec![0, 1, 3, 2];
+        } else {
+            panic!("Unsupported cell type.");
+        }
+        gmsh_s.push_str(" 2 0 0");
+        for j in 0..cell.len() {
             // currently assumes that Geometry and Topology use the same order
-            let vertex = grid.topology().cell(i).unwrap()[j];
+            let vertex = cell[vertex_order[j]];
             gmsh_s.push_str(&format!(" {vertex}"))
         }
         gmsh_s.push_str("\n");
@@ -42,11 +57,49 @@ pub fn export_as_gmsh(grid: SerialGrid, fname: String) {
 #[cfg(test)]
 mod test {
     use crate::io::*;
-    pub use crate::shapes::regular_sphere;
+    use crate::shapes::regular_sphere;
+    use solvers_tools::arrays::AdjacencyList;
+    use solvers_tools::arrays::Array2D;
 
     #[test]
-    fn test_gmsh_output() {
+    fn test_gmsh_output_regular_sphere() {
         let g = regular_sphere(2);
         export_as_gmsh(g, String::from("test_io_sphere.msh"));
+    }
+
+    #[test]
+    fn test_gmsh_output_quads() {
+        let g = SerialGrid::new(
+            Array2D::from_data(
+                vec![
+                    0.0, 0.0, 0.5, 0.0, 1.0, 0.0, 0.0, 0.5, 0.5, 0.5, 1.0, 0.5, 0.0, 1.0, 0.5, 1.0,
+                    1.0, 1.0,
+                ],
+                (9, 2),
+            ),
+            AdjacencyList::from_data(
+                vec![0, 1, 3, 4, 3, 4, 6, 7, 1, 2, 4, 5, 4, 5, 7, 8],
+                vec![0, 4, 8, 12, 16],
+            ),
+        );
+        export_as_gmsh(g, String::from("test_io_screen.msh"));
+    }
+
+    #[test]
+    fn test_gmsh_output_mixed_cell_type() {
+        let g = SerialGrid::new(
+            Array2D::from_data(
+                vec![
+                    0.0, 0.0, 0.5, 0.0, 1.0, 0.0, 0.0, 0.5, 0.5, 0.5, 1.0, 0.5, 0.0, 1.0, 0.5, 1.0,
+                    1.0, 1.0,
+                ],
+                (9, 2),
+            ),
+            AdjacencyList::from_data(
+                vec![0, 1, 4, 0, 4, 3, 1, 2, 4, 5, 3, 4, 7, 3, 7, 6, 4, 5, 7, 8],
+                vec![0, 3, 6, 10, 13, 16, 20],
+            ),
+        );
+        export_as_gmsh(g, String::from("test_io_screen_mixed.msh"));
     }
 }
