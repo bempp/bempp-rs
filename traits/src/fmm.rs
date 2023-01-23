203e78b5df2cfb6de7524a0d4edc3cb7347f6e76
Srinath Kailasa <srinathkailasa@gmail.com>
2023-01-23 16:03:19 +0000

    Remove old fmm traits

--- traits/src/fmm.rs
+++ traits/src/fmm.rs
@@ -1,23 +1,22 @@
 //! FMM traits
+pub trait Translation {
+    type Node;
 
-// pub trait Translation {
-//     type Node: FmmNode;
+    // Particle to Multipole
+    fn p2m(node: &mut Self::Node);
 
-//     // Particle to Multipole
-//     fn p2m(node: &mut Self::Node);
+    // Multipole to Multipole
+    fn m2m(in_node: &Self::Node, out_node: &mut Self::Node);
 
-//     // Multipole to Multipole
-//     fn m2m(in_node: &Self::Node, out_node: &mut Self::Node);
+    // Multipole to Local
+    fn m2l(in_node: &Self::Node, out_node: &mut Self::Node);
 
-//     // Multipole to Local
-//     fn m2l(in_node: &Self::Node, out_node: &mut Self::Node);
+    // Local to Local
+    fn l2l(in_node: &Self::Node, out_node: &mut Self::Node);
 
-//     // Local to Local
-//     fn l2l(in_node: &Self::Node, out_node: &mut Self::Node);
+    // Multipole to Particle
+    fn m2p(in_node: &Self::Node, out_node: &mut Self::Node);
 
-//     // Multipole to Particle
-//     fn m2p(in_node: &Self::Node, out_node: &mut Self::Node);
-
-//     // Local to Particle
-//     fn l2p(node: &mut Self::Node);
-// }
+    // Local to Particle
+    fn l2p(node: &mut Self::Node);
+}
