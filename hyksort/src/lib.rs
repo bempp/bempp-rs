ff54fc13e1e315c84c631cf554f5748f64fe6104
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-01-24 15:19:30 +0000

    \[ and \]

--- hyksort/src/lib.rs
+++ hyksort/src/lib.rs
@@ -1,10 +1,10 @@
-//! Hyksort [1] implementation in Rust, a near direct port of [2].
+//! Hyksort \[1\] implementation in Rust, a near direct port of \[2\].
 //!
 //! **References**
 //!
-//! [1] Sundar, H., Malhotra, D., & Biros, G. (2013, June). Hyksort: a new variant of hypercube quicksort on distributed memory architectures. In Proceedings of the 27th international ACM conference on international conference on supercomputing (pp. 293-302).
+//! \[1\] Sundar, H., Malhotra, D., & Biros, G. (2013, June). Hyksort: a new variant of hypercube quicksort on distributed memory architectures. In Proceedings of the 27th international ACM conference on international conference on supercomputing (pp. 293-302).
 //!
-//! [2] https://github.com/hsundar/usort
+//! \[2\] https://github.com/hsundar/usort
 #![cfg_attr(feature = "strict", deny(warnings))]
 
 extern crate superslice;
