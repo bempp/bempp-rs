2e0a21b392511889e2ab6ec850156a6fc844efe7
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-03-20 14:16:47 +0000

    move trait to bempp_traits

--- bem/src/dofmap.rs
+++ bem/src/dofmap.rs
@@ -1,23 +1,7 @@
 use bempp_tools::arrays::{AdjacencyList, Array2D};
+use bempp_traits::bem::DofMap;
 use bempp_traits::element::FiniteElement;
 use bempp_traits::grid::{Grid, Topology};
-
-pub trait DofMap {
-    /// Get the DOF numbers on the local process associated with the given entity
-    fn get_local_dof_numbers(&self, entity_dim: usize, entity_number: usize) -> &[usize];
-
-    /// Get the global DOF numbers associated with the given entity
-    fn get_global_dof_numbers(&self, entity_dim: usize, entity_number: usize) -> &[usize];
-
-    /// Get the number of DOFs associated with the local process
-    fn local_size(&self) -> usize;
-
-    /// Get the number of DOFs on all processes
-    fn global_size(&self) -> usize;
-
-    /// Get the local DOF numbers associated with a cell
-    fn cell_dofs(&self, cell: usize) -> Option<&[usize]>;
-}
 
 pub struct SerialDofMap {
     entity_dofs: [AdjacencyList<usize>; 4],
