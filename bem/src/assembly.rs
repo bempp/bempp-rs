e1e871f6b4fc04ebb663af1b8bcc8735caa1eaf5
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-05-04 11:37:06 +0100

    Implement helmholtz kernels (#93)
    * pass parameters into Green's function
    
    * Add Scalar template trait to Green's functions
    
    * template dense assembly
    
    * Allow complex and real assembly
    
    * add test data for Helmholtz
    
    * Helmholtz double and adjoint double
    
    * Helmholtz hypersingular
    
    * Add example showing user interface for assembly
    
    * use structs and trait instead of functions with params input

--- bem/src/assembly.rs
+++ bem/src/assembly.rs
@@ -1,5 +1,6 @@
 pub mod dense;
 use crate::green;
+use crate::green::Scalar;
 use bempp_tools::arrays::Array2D;
 use bempp_traits::bem::FunctionSpace;
 
@@ -14,63 +15,109 @@ pub enum BoundaryOperator {
     MagneticField,
 }
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 #[repr(u8)]
 pub enum PDEType {
     Laplace,
+    Helmholtz(f64),
 }
 
 // TODO: template over float type
 
 /// Assemble an operator into a dense matrix
-pub fn assemble_dense<'a>(
+pub fn assemble_dense<'a, T: Scalar>(
     // TODO: ouput should be `&mut impl ArrayAccess2D` once such a trait exists
-    output: &mut Array2D<f64>,
+    output: &mut Array2D<T>,
     operator: BoundaryOperator,
     pde: PDEType,
     trial_space: &impl FunctionSpace<'a>,
     test_space: &impl FunctionSpace<'a>,
 ) {
-    let kernel = match pde {
+    match pde {
         PDEType::Laplace => match operator {
-            BoundaryOperator::SingleLayer => green::laplace_green,
-            BoundaryOperator::DoubleLayer => green::laplace_green_dy,
-            BoundaryOperator::AdjointDoubleLayer => green::laplace_green_dx,
-            BoundaryOperator::Hypersingular => green::laplace_green,
+            BoundaryOperator::SingleLayer => {
+                dense::assemble(
+                    output,
+                    &green::LaplaceGreenKernel {},
+                    false,
+                    false,
+                    trial_space,
+                    test_space,
+                );
+            }
+            BoundaryOperator::DoubleLayer => {
+                dense::assemble(
+                    output,
+                    &green::LaplaceGreenDyKernel {},
+                    false,
+                    true,
+                    trial_space,
+                    test_space,
+                );
+            }
+            BoundaryOperator::AdjointDoubleLayer => {
+                dense::assemble(
+                    output,
+                    &green::LaplaceGreenDxKernel {},
+                    true,
+                    false,
+                    trial_space,
+                    test_space,
+                );
+            }
+            BoundaryOperator::Hypersingular => {
+                dense::laplace_hypersingular_assemble(output, trial_space, test_space);
+            }
+            _ => {
+                panic!("Invalid operator");
+            }
+        },
+        PDEType::Helmholtz(k) => match operator {
+            BoundaryOperator::SingleLayer => {
+                dense::assemble(
+                    output,
+                    &green::HelmholtzGreenKernel { k },
+                    false,
+                    false,
+                    trial_space,
+                    test_space,
+                );
+            }
+            BoundaryOperator::DoubleLayer => {
+                dense::assemble(
+                    output,
+                    &green::HelmholtzGreenDyKernel { k },
+                    false,
+                    true,
+                    trial_space,
+                    test_space,
+                );
+            }
+            BoundaryOperator::AdjointDoubleLayer => {
+                dense::assemble(
+                    output,
+                    &green::HelmholtzGreenDxKernel { k },
+                    true,
+                    false,
+                    trial_space,
+                    test_space,
+                );
+            }
+            BoundaryOperator::Hypersingular => {
+                dense::helmholtz_hypersingular_assemble(output, trial_space, test_space, k);
+            }
             _ => {
                 panic!("Invalid operator");
             }
         },
     };
-    let needs_trial_normal = match operator {
-        BoundaryOperator::DoubleLayer => true,
-        _ => false,
-    };
-    let needs_test_normal = match operator {
-        BoundaryOperator::AdjointDoubleLayer => true,
-        _ => false,
-    };
-
-    if operator == BoundaryOperator::Hypersingular {
-        dense::hypersingular_assemble(output, kernel, trial_space, test_space);
-    } else {
-        dense::assemble(
-            output,
-            kernel,
-            needs_trial_normal,
-            needs_test_normal,
-            trial_space,
-            test_space,
-        );
-    }
 }
-
 #[cfg(test)]
 mod test {
     use crate::assembly::dense;
     use crate::assembly::*;
     use crate::function_space::SerialFunctionSpace;
-    use crate::green::laplace_green;
+    use crate::green::{HelmholtzGreenKernel, LaplaceGreenKernel};
     use approx::*;
     use bempp_element::element::create_element;
     use bempp_grid::shapes::regular_sphere;
@@ -79,25 +126,7 @@ mod test {
     use bempp_traits::bem::DofMap;
     use bempp_traits::cell::ReferenceCellType;
     use bempp_traits::element::ElementFamily;
-
-    fn laplace_single_layer<'a>(
-        trial_space: &impl FunctionSpace<'a>,
-        test_space: &impl FunctionSpace<'a>,
-    ) -> Array2D<f64> {
-        let mut output = Array2D::<f64>::new((
-            test_space.dofmap().global_size(),
-            trial_space.dofmap().global_size(),
-        ));
-        dense::assemble(
-            &mut output,
-            laplace_green,
-            false,
-            false,
-            trial_space,
-            test_space,
-        );
-        output
-    }
+    use num::complex::Complex;
 
     #[test]
     fn test_laplace_single_layer() {
@@ -117,7 +146,16 @@ mod test {
         let space0 = SerialFunctionSpace::new(&grid, &element0);
         let space1 = SerialFunctionSpace::new(&grid, &element1);
 
-        let matrix = laplace_single_layer(&space0, &space1);
+        let mut matrix =
+            Array2D::<f64>::new((space1.dofmap().global_size(), space0.dofmap().global_size()));
+        dense::assemble(
+            &mut matrix,
+            &LaplaceGreenKernel {},
+            false,
+            false,
+            &space0,
+            &space1,
+        );
 
         let mut matrix2 =
             Array2D::<f64>::new((space1.dofmap().global_size(), space0.dofmap().global_size()));
@@ -134,7 +172,67 @@ mod test {
             for j in 0..space0.dofmap().global_size() {
                 assert_relative_eq!(
                     *matrix.get(i, j).unwrap(),
-                    matrix2.get(i, j).unwrap(),
+                    *matrix2.get(i, j).unwrap(),
+                    epsilon = 0.0001
+                );
+            }
+        }
+    }
+
+    #[test]
+    fn test_helmholtz_single_layer() {
+        let grid = regular_sphere(1);
+        let element0 = create_element(
+            ElementFamily::Lagrange,
+            ReferenceCellType::Triangle,
+            0,
+            true,
+        );
+        let element1 = create_element(
+            ElementFamily::Lagrange,
+            ReferenceCellType::Triangle,
+            1,
+            false,
+        );
+        let space0 = SerialFunctionSpace::new(&grid, &element0);
+        let space1 = SerialFunctionSpace::new(&grid, &element1);
+
+        let mut matrix = Array2D::<Complex<f64>>::new((
+            space1.dofmap().global_size(),
+            space0.dofmap().global_size(),
+        ));
+        dense::assemble(
+            &mut matrix,
+            &HelmholtzGreenKernel { k: 2.5 },
+            false,
+            false,
+            &space0,
+            &space1,
+        );
+
+        let mut matrix2 = Array2D::<Complex<f64>>::new((
+            space1.dofmap().global_size(),
+            space0.dofmap().global_size(),
+        ));
+
+        assemble_dense(
+            &mut matrix2,
+            BoundaryOperator::SingleLayer,
+            PDEType::Helmholtz(2.5),
+            &space0,
+            &space1,
+        );
+
+        for i in 0..space1.dofmap().global_size() {
+            for j in 0..space0.dofmap().global_size() {
+                assert_relative_eq!(
+                    matrix.get(i, j).unwrap().re,
+                    matrix2.get(i, j).unwrap().re,
+                    epsilon = 0.0001
+                );
+                assert_relative_eq!(
+                    matrix.get(i, j).unwrap().im,
+                    matrix2.get(i, j).unwrap().im,
                     epsilon = 0.0001
                 );
             }
