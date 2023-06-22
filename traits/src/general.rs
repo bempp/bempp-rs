6e34156d8a6ebad3e2cf793fe7c93ba13370c224
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-06-22 11:45:08 +0100

    fix more clippy errors

--- traits/src/general.rs
+++ traits/src/general.rs
@@ -1,20 +1,26 @@
 //! Traits useful across packages.
 
-// An indexable data view.
+/// An indexable data view.
 pub trait IndexableView {
-    // The Item type
+    /// The Item type
     type Item;
 
-    // Get data at position.
+    /// Get data at position.
     fn get(&self, index: usize) -> Option<&Self::Item>;
 
-    // Get mutable data at position.
+    /// Get mutable data at position.
     fn get_mut(&mut self, index: usize) -> Option<&mut Self::Item>;
 
-    // Get unchecked access to data.
+    /// Get unchecked access to data.
+    ///
+    /// # Safety
+    /// This function does not complete bound checks
     unsafe fn get_unchecked(&self, index: usize) -> Option<&Self::Item>;
 
-    // Get unchecked mutable access to data.
+    /// Get unchecked mutable access to data.
+    ///
+    /// # Safety
+    /// This function does not complete bound checks
     unsafe fn get_unchecked_mut(&self, index: usize) -> Option<&mut Self::Item>;
 
     // TODO: Iterator
