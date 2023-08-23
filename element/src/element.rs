eacbf9c73570c8a1b8c69850079dddbc659c51fa
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-08-23 10:19:43 +0100

    implement tabulte with Legendre basis

--- element/src/element.rs
+++ element/src/element.rs
@@ -5,10 +5,10 @@ use bempp_traits::arrays::{AdjacencyListAccess, Array2DAccess, Array3DAccess, Ar
 use bempp_traits::cell::ReferenceCellType;
 use bempp_traits::element::{ElementFamily, FiniteElement, MapType};
 use crate::cell::create_cell;
-use crate::polynomials::{polynomial_count, tabulate_legendre_polynomials};
+use crate::polynomials::{polynomial_count, tabulate_legendre_polynomials, legendre_shape};
 use rlst_dense::RandomAccessMut;
 use rlst_algorithms::linalg::LinAlg;
-use rlst_algorithms::lapack::inverse;
+use rlst_algorithms::traits::inverse::Inverse;
 pub mod lagrange;
 pub mod raviart_thomas;
 
@@ -197,153 +197,17 @@ impl FiniteElement for CiarletElement {
     fn tabulate<'a>(
         &self,
         points: &impl Array2DAccess<'a, f64>,
-        _nderivs: usize,
+        nderivs: usize,
         data: &mut impl Array4DAccess<f64>,
     ) {
-        for deriv in 0..data.shape().0 {
-            for pt in 0..data.shape().1 {
-                let evals = match self.cell_type {
-                    ReferenceCellType::Interval => {
-                        let x = *points.get(pt, 0).unwrap();
-                        match self.highest_degree {
-                            0 => match deriv {
-                                0 => vec![1.0],
-                                _ => vec![0.0],
-                            },
-                            1 => match deriv {
-                                0 => vec![1.0, x],
-                                1 => vec![0.0, 1.0],
-                                _ => vec![0.0, 0.0, 0.0],
-                            },
-                            2 => match deriv {
-                                0 => vec![1.0, x, x * x],
-                                1 => vec![0.0, 1.0, 2.0 * x],
-                                2 => vec![0.0, 0.0, 2.0],
-                                _ => vec![0.0, 0.0, 0.0],
-                            },
-                            3 => match deriv {
-                                0 => vec![1.0, x, x * x, x * x * x],
-                                1 => vec![0.0, 1.0, 2.0 * x, 3.0 * x * x],
-                                2 => vec![0.0, 0.0, 2.0, 6.0 * x],
-                                3 => vec![0.0, 0.0, 0.0, 6.0],
-                                _ => vec![0.0, 0.0, 0.0, 0.0],
-                            },
-                            _ => {
-                                panic!("Degree currently unsupported");
-                            }
-                        }
-                    }
-                    ReferenceCellType::Triangle => {
-                        let x = *points.get(pt, 0).unwrap();
-                        let y = *points.get(pt, 1).unwrap();
-                        match self.highest_degree {
-                            0 => match deriv {
-                                0 => vec![1.0],
-                                _ => vec![0.0],
-                            },
-                            1 => match deriv {
-                                0 => vec![1.0, x, y],
-                                1 => vec![0.0, 1.0, 0.0],
-                                2 => vec![0.0, 0.0, 1.0],
-                                _ => vec![0.0, 0.0, 0.0],
-                            },
-                            2 => match deriv {
-                                0 => vec![1.0, x, y, x * x, x * y, y * y],
-                                1 => vec![0.0, 1.0, 0.0, 2.0 * x, y, 0.0],
-                                2 => vec![0.0, 0.0, 1.0, 0.0, x, 2.0 * y],
-                                3 => vec![0.0, 0.0, 0.0, 2.0, 0.0, 0.0],
-                                4 => vec![0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
-                                5 => vec![0.0, 0.0, 0.0, 0.0, 0.0, 2.0],
-                                _ => vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                            },
-                            _ => {
-                                panic!("Degree currently unsupported");
-                            }
-                        }
-                    }
-                    ReferenceCellType::Quadrilateral => {
-                        let x = *points.get(pt, 0).unwrap();
-                        let y = *points.get(pt, 1).unwrap();
-                        match self.highest_degree {
-                            0 => match deriv {
-                                0 => vec![1.0],
-                                _ => vec![0.0],
-                            },
-                            1 => match deriv {
-                                0 => vec![1.0, x, y, x * y],
-                                1 => vec![0.0, 1.0, 0.0, y],
-                                2 => vec![0.0, 0.0, 1.0, x],
-                                4 => vec![0.0, 0.0, 0.0, 1.0],
-                                _ => vec![0.0, 0.0, 0.0, 0.0],
-                            },
-                            2 => match deriv {
-                                0 => vec![
-                                    1.0,
-                                    x,
-                                    x * x,
-                                    y,
-                                    x * y,
-                                    x * x * y,
-                                    y * y,
-                                    x * y * y,
-                                    x * x * y * y,
-                                ],
-                                1 => vec![
-                                    0.0,
-                                    1.0,
-                                    2.0 * x,
-                                    0.0,
-                                    y,
-                                    2.0 * x * y,
-                                    0.0,
-                                    y * y,
-                                    2.0 * x * y * y,
-                                ],
-                                2 => vec![
-                                    0.0,
-                                    0.0,
-                                    0.0,
-                                    1.0,
-                                    x,
-                                    x * x,
-                                    2.0 * y,
-                                    2.0 * x * y,
-                                    2.0 * x * x * y,
-                                ],
-                                3 => vec![0.0, 0.0, 2.0, 0.0, 0.0, 2.0 * y, 0.0, 0.0, 2.0 * y * y],
-                                4 => vec![
-                                    0.0,
-                                    0.0,
-                                    0.0,
-                                    0.0,
-                                    1.0,
-                                    2.0 * x,
-                                    0.0,
-                                    2.0 * y,
-                                    4.0 * x * y,
-                                ],
-                                5 => vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.0, 2.0 * x, 2.0 * x * x],
-                                7 => vec![0.0, 0.0, 0.0, 0.0, 0.0, 2.0, 0.0, 0.0, 4.0 * y],
-                                8 => vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.0, 4.0 * x],
-                                12 => vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 4.0],
-                                _ => vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-                            },
-                            _ => {
-                                panic!("Degree currently unsupported");
-                            }
-                        }
-                    }
-                    _ => {
-                        panic!("Tabulation not implemented for this cell type");
-                    }
-                };
-
-                for i in 0..self.coefficients.shape().0 {
-                    for j in 0..self.coefficients.shape().1 {
-                        *data.get_mut(deriv, pt, i, j).unwrap() = 0.0;
-                        for (k, e) in evals.iter().enumerate() {
-                            *data.get_mut(deriv, pt, i, j).unwrap() +=
-                                unsafe { *self.coefficients.get_unchecked(i, j, k) } * *e;
+        let mut table = Array3D::<f64>::new(legendre_shape(self.cell_type, points, self.highest_degree, nderivs));
+        tabulate_legendre_polynomials(self.cell_type, points, self.highest_degree, nderivs, &mut table);
+        for d in 0..table.shape().0 {
+            for i in 0..table.shape().1 {
+                for p in 0..points.shape().0 {
+                    for j in 0..self.value_size {
+                        for b in 0..self.dim {
+                            *data.get_mut(d, p, b, j).unwrap() += *self.coefficients.get(b, j, p).unwrap() * *table.get_mut(d, i, p).unwrap();
                         }
                     }
                 }
