46e2d58c3611274db5d8f3005d48ec463f846512
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-03-19 10:40:36 +0000

    update benchmark code

--- bem/benches/assembly_benchmark.rs
+++ bem/benches/assembly_benchmark.rs
@@ -1,10 +1,9 @@
 use bempp_bem::assembly::{batched, batched::BatchedAssembler};
 use bempp_bem::function_space::SerialFunctionSpace;
-use bempp_element::element::lagrange;
+use bempp_element::element::LagrangeElementFamily;
 use bempp_grid::shapes::regular_sphere;
 use bempp_traits::bem::FunctionSpace;
 use bempp_traits::element::Continuity;
-use bempp_traits::types::ReferenceCellType;
 use criterion::{criterion_group, criterion_main, Criterion};
 use rlst::rlst_dynamic_array2;
 
@@ -17,12 +16,12 @@ pub fn assembly_parts_benchmark(c: &mut Criterion) {
 
     for i in 3..5 {
         let grid = regular_sphere(i);
-        let element = lagrange::create(ReferenceCellType::Triangle, 0, Continuity::Discontinuous);
+        let element = LagrangeElementFamily::<f64>::new(0, Continuity::Discontinuous);
 
         let space = SerialFunctionSpace::new(&grid, &element);
         let mut matrix = rlst_dynamic_array2!(f64, [space.global_size(), space.global_size()]);
 
-        let colouring = space.compute_cell_colouring();
+        let colouring = space.cell_colouring();
         let a = batched::LaplaceSingleLayerAssembler::<128, f64>::default();
 
         group.bench_function(
