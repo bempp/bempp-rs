b9a347f3b91b74f178f767981a340279ef9ed523
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-05-10 15:26:19 +0100

    remove parallel_grid (to be reverted)

--- src/grid.rs
+++ src/grid.rs
@@ -4,8 +4,6 @@ pub mod common;
 pub mod flat_triangle_grid;
 pub mod io;
 pub mod mixed_grid;
-#[cfg(feature = "mpi")]
-pub mod parallel_grid;
 pub mod shapes;
 pub mod single_element_grid;
 pub mod traits;
