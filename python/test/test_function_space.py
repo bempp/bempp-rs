1228a83f0c482d6d7ac6d22ef8490d8947601989
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-09-18 11:07:26 +0100

    add tests

--- python/test/test_function_space.py
+++ python/test/test_function_space.py
@@ -14,6 +14,7 @@ def test_create_space_dp0(level):
 
     assert space.local_size == grid.entity_count(ReferenceCellType.Triangle)
     assert space.local_size == space.global_size
+    assert space.is_serial
 
 
 @pytest.mark.parametrize("level", range(4))
@@ -24,6 +25,7 @@ def test_create_space_p1(level):
 
     assert space.local_size == grid.entity_count(ReferenceCellType.Point)
     assert space.local_size == space.global_size
+    assert space.is_serial
 
 
 @pytest.mark.parametrize("level", range(4))
@@ -36,6 +38,7 @@ def test_create_space_p2(level):
         ReferenceCellType.Interval
     )
     assert space.local_size == space.global_size
+    assert space.is_serial
 
 
 @pytest.mark.parametrize("level", range(4))
@@ -64,3 +67,53 @@ def test_element(level):
     e = ReferenceCellType.Triangle
     assert space.element(e).dim == element.element(e).dim
     assert space.element(e).entity_dofs(0, 0) == element.element(e).entity_dofs(0, 0)
+
+
+@pytest.mark.parametrize("level", range(4))
+@pytest.mark.parametrize("degree", range(1, 5))
+def test_get_local_dof_numbers(level, degree):
+    grid = regular_sphere(level)
+    element = create_family(Family.Lagrange, degree)
+    space = function_space(grid, element)
+
+    for i in range(grid.entity_count(ReferenceCellType.Point)):
+        assert len(space.get_local_dof_numbers(0, i)) == 1
+    for i in range(grid.entity_count(ReferenceCellType.Interval)):
+        assert len(space.get_local_dof_numbers(1, i)) == degree - 1
+    for i in range(grid.entity_count(ReferenceCellType.Triangle)):
+        assert len(space.get_local_dof_numbers(2, i)) == (degree - 1) * (degree - 2) // 2
+
+
+@pytest.mark.parametrize("level", range(4))
+@pytest.mark.parametrize("degree", range(1, 5))
+def test_cell_dofs(level, degree):
+    grid = regular_sphere(level)
+    element = create_family(Family.Lagrange, degree)
+    space = function_space(grid, element)
+
+    for i in range(grid.entity_count(ReferenceCellType.Triangle)):
+        assert len(space.cell_dofs(i)) == (degree + 1) * (degree + 2) // 2
+
+
+@pytest.mark.parametrize("level", range(4))
+@pytest.mark.parametrize("degree", range(1, 5))
+def test_global_dofs(level, degree):
+    grid = regular_sphere(level)
+    element = create_family(Family.Lagrange, degree)
+    space = function_space(grid, element)
+
+    assert space.local_size == space.global_size
+    for i in range(space.local_size):
+        assert space.global_dof_index(i) == i
+
+
+@pytest.mark.parametrize("level", range(4))
+@pytest.mark.parametrize("degree", range(1, 5))
+def test_ownership(level, degree):
+    grid = regular_sphere(level)
+    element = create_family(Family.Lagrange, degree)
+    space = function_space(grid, element)
+
+    assert space.local_size == space.global_size
+    for i in range(space.local_size):
+        assert space.ownership(i).is_owned
